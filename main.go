1b070e949d1acb27d7084801c567963518aca0d7
VictorMonte <victorbcm@hotmail.com>
2017-09-25 20:10:45 -0300

    Syncing routes

--- main.go
+++ main.go
@@ -7,7 +7,27 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
+
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
