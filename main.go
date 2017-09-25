8816076633fdf25c61539b0526839afdfc0939db
VictorMonte <victorbcm@hotmail.com>
2017-09-25 20:32:22 -0300

    Implement get people

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -26,7 +27,7 @@ func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
 }
 
 func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
-
+	json.NewEncoder(w).Encode(people)
 }
 
 func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
